0f3e309c9c3181fb84ebd4d7fca76c91a9898128
Luca Morelli <80129307+MorelliLuca@users.noreply.github.com>
2021-10-19 23:03:33 +0200

    Added comments

--- ParticleType.hpp
+++ ParticleType.hpp
@@ -1,3 +1,5 @@
+//Header file ParticleType.hpp - Luca Morelli 2021
+
 #ifndef PARTICLETYPE_HPP
 #define PARTICLETYPE_HPP
 
@@ -5,19 +7,24 @@
 #include <iostream>
 
 class ParticleType {
-  std::string const name_;
+  //Data Members
+  std::string const name_; 
   double const mass_;
   int const charge_;
 
 public:
+  //Constructor declaration
   ParticleType(std::string name, double mass, int charge)
       : name_{name}, mass_{mass}, charge_{charge} {}
+  
+  //Member function declaration
   virtual void print() const;
   std::string const &getName() const;
   double const &getMass() const;
   int const &getCharge() const;
 };
 
+//Operator overload
 std::ostream& operator<<(std::ostream& os,ParticleType const& particleType); 
 
 #endif
