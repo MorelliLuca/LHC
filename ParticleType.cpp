75c2546e4b095270fca84d3eff5f0c7d049be703
Luca Morelli <80129307+MorelliLuca@users.noreply.github.com>
2021-10-19 23:10:53 +0200

    Update ParticleType.cpp

--- ParticleType.cpp
+++ ParticleType.cpp
@@ -1,7 +1,10 @@
+//Implementation of ParticleType.hpp - Luca Morelli 2021
+
 #include "ParticleType.hpp"
 #include <iostream>
 #include <string>
 
+//Member function definitions
 void ParticleType::print() const {
   std::cout << '|' << name_ << "|Mass:" << mass_ << "Kg|Charge:" << charge_
             << "C|";
@@ -13,6 +16,7 @@ double const &ParticleType::getMass() const { return mass_; }
 
 int const &ParticleType::getCharge() const { return charge_; }
 
+//Operator overload definition
 std::ostream& operator<<(std::ostream& os,ParticleType const& particleType){
   particleType.print();
   return os;
