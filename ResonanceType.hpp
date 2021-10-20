2427ae5c2c1cf290f889af9cddab0646bfb05f36
Luca Morelli <80129307+MorelliLuca@users.noreply.github.com>
2021-10-20 08:04:59 +0200

    Added comments

--- ResonanceType.hpp
+++ ResonanceType.hpp
@@ -1,20 +1,25 @@
+//Header file ResonanceType.hpp - Luca Morelli 2021
 #ifndef RESONANCETYPE_HPP
 #define RESONANCETYPE_HPP
 
 #include "ParticleType.hpp"
 #include <string>
 
+//ResonanceType class inherits from ParticleType
 class ResonanceType : public ParticleType {
+  //Data member
   double const width_;
 
 public:
-  //  ParticleType() {}
+  //  Constructor
   ResonanceType(std::string name, double mass, int charge, double width)
       : ParticleType(name, mass, charge), width_{width} {}   
+  /Member function
   void print() const;
   double const &getWidth() const;
 };
 
+//Operator overload
 std::ostream& operator<<(std::ostream& os,ResonanceType const& resonanceType); 
 
 #endif
